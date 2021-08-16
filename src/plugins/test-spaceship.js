f09eef6aa13c81cd4d5c16de5bf7c59a10b3700a
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2021-08-16 09:33:32 -0700

    test spaceship fix

--- src/plugins/test-spaceship.js
+++ src/plugins/test-spaceship.js
@@ -1,31 +1,28 @@
-import {fullKey, getFullKey} from "./utils";
+import { fullKey, getFullKey } from './utils'
 
-var spaceship_data = new Map();
-require("./test-spaceship.json").forEach(domain_object => {
-    const full_key = getFullKey(domain_object);
-    spaceship_data.set(full_key, domain_object);
-});
+const spaceship_data = new Map()
+require('./test-spaceship.json').forEach((domain_object) => {
+  const full_key = getFullKey(domain_object)
+  spaceship_data.set(full_key, domain_object)
+})
 
-class DomainObjectProvider{
-    get(identifier) {
-        const full_key = fullKey(identifier);
-        return spaceship_data.get(full_key);
-    }
-}
+export default function TestSpaceshipPlugin () {
+  return function install (openmct) {
+    console.log('Test spaceship enabled')
+    openmct.objects.addRoot({
+      namespace: 'test-spaceship',
+      key: 'root'
+    })
+    openmct.objects.addProvider('test-spaceship', {
+      get: function (identifier) {
+        return Promise.resolve(spaceship_data.get(fullKey(identifier)))
+      }
+    })
 
-export default function TestSpaceshipPlugin(){
-    return function install(openmct) {
-        console.log("Test spaceship enabled");
-        openmct.objects.addRoot({
-            namespace: "test-spaceship",
-            key: "root"
-        });
-        openmct.objects.addProvider("test-spaceship", new DomainObjectProvider());
-
-        openmct.types.addType("example.telemetry", {
-            name: "Example Telemetry Point",
-            description: "Example telemetry point from our happy tutorial.",
-            cssClass: "icon-telemetry"
-        });
-    };
+    openmct.types.addType('example.telemetry', {
+      name: 'Example Telemetry Point',
+      description: 'Example telemetry point from our happy tutorial.',
+      cssClass: 'icon-telemetry'
+    })
+  }
 }
