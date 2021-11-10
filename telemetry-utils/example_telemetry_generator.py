69a93c8fca9331e592eb4d850bf7bdccb5dcf59b
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2021-11-10 19:26:05 +0000

    internal host

--- telemetry-utils/example_telemetry_generator.py
+++ telemetry-utils/example_telemetry_generator.py
@@ -18,5 +18,8 @@ async def run_client(uri):
 
 
 if __name__ == "__main__":
-    asyncio.ensure_future(run_client("ws://localhost:8080/injest/test-spaceship.fuel"))
+    # 172.17.0.1 is the default IP for host if running inside Docker container
+    # If running on Docker Desktop on Windows, you might need to use host.docker.internal
+    # For production, create a container for Python apps and attach it to the network with docker-compose
+    asyncio.ensure_future(run_client("ws://172.17.0.1:8080/injest/test-spaceship.fuel"))
     asyncio.get_event_loop().run_forever()
